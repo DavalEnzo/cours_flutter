d47c70bc5a8c0036477803cb100a427d21d5de76
Enzo Daval <81628324+DavalEnzo@users.noreply.github.com>
2023-07-22 01:03:22 +0200

    Actualisation position favoris (#10)
    * ajout envoi de localisations en arrière plan si accepté par l'utilisateur
    
    * suppression de la page carte et mise sur la page de base: actualisation dynamique de la position des favoris toutes les 10 secs

--- lib/view/my_map_view.dart
+++ lib/view/my_map_view.dart
@@ -1,9 +1,16 @@
 import 'dart:async';
 
-import 'package:cours_flutter/controller/permission_gps.dart';
-import 'package:cours_flutter/view/google_carte.dart';
+import 'package:cours_flutter/view/conversation_page.dart';
+import 'package:cours_flutter/view/user_details.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'dart:ui' as ui;
+
+import '../controller/firestore_helper.dart';
+import '../global.dart';
+import '../model/my_user.dart';
 
 class MyMapView extends StatefulWidget {
   const MyMapView({super.key});
@@ -13,41 +20,143 @@ class MyMapView extends StatefulWidget {
 }
 
 class _MyMapViewState extends State<MyMapView> {
-  Stream<Position> _positionStream = const Stream.empty();
+  // variable
+  Completer<GoogleMapController> completer = Completer();
+  List<MyUser> friendList = [];
+  List<Marker> markers = [];
+  LocationData? myPosition;
+  late CameraPosition camera;
+
+  getMyLocation() async {
+    Location location = Location();
+
+    LocationData value = await location.getLocation();
+    setState(() {
+      myPosition = value;
+      camera = CameraPosition(
+        target: LatLng(myPosition!.latitude!, myPosition!.longitude!),
+        zoom: 13,
+      );
+    });
+  }
+  Timer? _refreshTimer; // Declare the Timer variable.
 
   @override
   void initState() {
     super.initState();
-    _positionStream = Stream.periodic(const Duration(seconds: 1))
-        .asyncMap((_) => PermissionGps().init());
+    getMyLocation().then((_) {
+      getFriendsLocation();
+    });
+
+    // Start the periodic refresh and store the Timer in the _refreshTimer variable.
+    _refreshTimer = Timer.periodic(const Duration(seconds: 10), (Timer timer) {
+      _refreshFriendList();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel the timer when the widget is disposed to avoid calling setState after dispose.
+    _refreshTimer?.cancel();
+    super.dispose();
+  }
+
+  void _refreshFriendList() {
+    setState(() {
+      friendList.clear(); // Clear the existing friendList.
+      getFriendsLocation(); // Fetch the updated friendList.
+    });
+  }
+
+  Future<void> getFriendsLocation() async {
+    for (String index in me.favoris!) {
+      MyUser user = await FirestoreHelper().getUser(index);
+      setState(() {
+        friendList.add(user);
+      });
+    }
+  }
+
+  Future<Uint8List> readNetworkImage(String imageUrl) async {
+    final ByteData data =
+    await NetworkAssetBundle(Uri.parse(imageUrl)).load(imageUrl);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: camera.zoom.toInt() + 90);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  createMarkers() async {
+    List<MyUser> friendListCopy = List.from(friendList); // Create a copy of the friendList.
+    List<Marker> updatedMarkers = []; // Create a new list for markers.
+
+    for (MyUser friend in friendListCopy) {
+      if (friend.coordonnees != null) {
+        Uint8List markerIcon =
+        await readNetworkImage(friend.avatar ?? defaultImage);
+        updatedMarkers.add(
+          Marker(
+            markerId: MarkerId(friend.id),
+            position: LatLng(friend.coordonnees!["latitude"], friend.coordonnees!["longitude"]),
+            icon: BitmapDescriptor.fromBytes(markerIcon),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ConversationPage(user: friend)));
+            },
+          ),
+        );
+      }
+    }
+
+    // Assign the new list of markers to the markers variable.
+    setState(() {
+      markers = updatedMarkers;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<Position>(
-      stream: _positionStream,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-              child: SizedBox(
-            width: 50,
-            height: 50,
-            child: CircularProgressIndicator(
-              color: Colors.white,
-              backgroundColor: Colors.deepOrange,
-              strokeWidth: 7,
-            ),
-          )
-          );
-        } else if (snapshot.hasError) {
-          return const Center(
-              child: Text("Erreur de chargement des données",
-                  style: TextStyle(color: Colors.white, fontSize: 35)));
-        } else {
-          Position location = snapshot.data!;
-          return CarteGoogle(location: location);
-        }
-      },
+    createMarkers();
+    return Scaffold(
+      body: myPosition == null
+          ? const Center(
+        child: CircularProgressIndicator(
+          color: Colors.white,
+          backgroundColor: Colors.deepOrange,
+          strokeWidth: 7,
+        ),
+      )
+          : GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(myPosition!.latitude!, myPosition!.longitude!),
+          zoom: 13,
+        ),
+        markers: {
+          Marker(
+            markerId: MarkerId(me.id),
+            position: LatLng(myPosition!.latitude!, myPosition!.longitude!),
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+            infoWindow: InfoWindow(title: "Moi", snippet: "Je suis ici", onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => UserDetails(userId: me.id)));
+            }),
+          ),
+          ...markers
+        },
+        buildingsEnabled: true,
+        myLocationEnabled: true,
+        onCameraMove: (position) {
+          setState(() {
+            camera = position;
+          });
+        },
+        onMapCreated: (GoogleMapController controller) async {
+          controller.setMapStyle("[]");
+          controller.setMapStyle(
+              await rootBundle.loadString('lib/map_style.json'));
+          completer.complete(controller);
+        },
+      ),
     );
   }
 }
